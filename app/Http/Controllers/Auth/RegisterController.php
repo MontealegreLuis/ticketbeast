8561e79e0cf4d7cebe038b39fad30ec4932ec43e
Luis Montealegre <montealegreluis@gmail.com>
2018-01-20 22:30:22 -0600

    fix: A promoter cannot register with unknown or used invitations

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -18,11 +18,15 @@ class RegisterController extends Controller
     public function register()
     {
         $invitation = Invitation::findByCode(request('invitation_code'));
+
+        abort_if($invitation->hasBeenUsed(), 404);
+
         $promoter = User::create([
             'email' => request('email'),
             'password' => Hash::make(request('password')),
         ]);
         $invitation->update(['user_id' => $promoter->id]);
+
         Auth::login($promoter);
 
         return redirect()->route('backstage.concerts.index');
