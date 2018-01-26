21c4cdbcb4d51bb66b7c804df18bd3acabf5b9d4
Luis Montealegre <montealegreluis@gmail.com>
2018-01-25 22:36:37 -0600

    feature: Promoters now receive invitations to join via email

--- app/Mail/InvitationEmail.php
+++ app/Mail/InvitationEmail.php
@@ -27,6 +27,9 @@ class InvitationEmail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this
+            ->view('emails.invitation-email')
+            ->subject('You\'re invited to join TicketBeast!')
+        ;
     }
 }
